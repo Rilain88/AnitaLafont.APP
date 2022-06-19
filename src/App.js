4b066d9cc8c0fbc8cda1bbef8722f82082d0aaa0
leoauza <leo_auza@hotmail.com>
2022-06-19 03:34:34 -0300

    Agrege Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Zapatos magicos para vestir
-        </h1>
-        <a
-          className="App-link"
-          href="http://anitalafont.000webhostapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Anita Lafont
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
