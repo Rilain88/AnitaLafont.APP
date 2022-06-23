5fa5bd943ea3257a7e3746d939fe9de644d4c116
leoauza <leo_auza@hotmail.com>
2022-06-23 00:34:11 -0300

    Agregue ItemCount.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ItemListContainer />
+      <ItemCount/>
     </div>
   );
 }
